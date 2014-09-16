a299f32efc754c9ad7eeb45c3cf7c667cc9344e1
Jon Radon <jradon@routeone.com>
2014-09-16 18:56:20 -0400

    Add project and starter class

---
+++ src/test/java/JerseyClientTrackerTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JerseyClientTrackerTest {
+	@Test
+	public void doesStuff() throws Exception {
+		assertTrue(false);
+	}
+}
