83c6b1566e1b563c736bb3be92dc6a877f4ebd0b
Peter Richards <parichards@gmail.com>
2014-09-16 23:18:27 -0400

    Start of implementation for Request Tracker Client Filter

--- src/main/java/RequestTrackerClientFilter.java
+++ src/main/java/RequestTrackerClientFilter.java
@@ -1,5 +1,23 @@
-/**
- * Created by Pete on 9/16/2014.
- */
-public class RequestTrackerClientFilter {
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.filter.ClientFilter;
+
+import java.util.UUID;
+
+
+public class RequestTrackerClientFilter extends ClientFilter {
+
+    @Override
+    public ClientResponse handle(ClientRequest clientRequest) throws ClientHandlerException {
+        // As getNext() is final in ClientFilter and cannot be stubbed in test classes, all work is
+        // performed in the protected doWork method which can be unit tested.
+        // That the filter hands off to the next in the chain can only be tested in an integration test
+        return getNext().handle(doWork(clientRequest));
+    }
+
+    protected ClientRequest doWork(ClientRequest clientRequest) {
+        clientRequest.getHeaders().add("X-Request-Tracker", UUID.randomUUID());
+        return clientRequest;
+    }
 }
--- src/test/java/RequestTrackerClientFilterTest.java
+++ src/test/java/RequestTrackerClientFilterTest.java
@@ -1,5 +1,46 @@
-/**
- * Created by Pete on 9/16/2014.
- */
+import com.sun.jersey.api.client.ClientHandler;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.filter.ClientFilter;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+
 public class RequestTrackerClientFilterTest {
+    private RequestTrackerClientFilter requestTrackerClientFilter;
+
+    private ClientRequest clientRequest = mock(ClientRequest.class);
+    private MultivaluedMap<String,Object> headersMap = mock(MultivaluedMap.class);
+
+    @Before
+    public void setUp() {
+        requestTrackerClientFilter = new RequestTrackerClientFilter();
+        when(clientRequest.getHeaders()).thenReturn(headersMap);
+    }
+
+    @Test
+    public void returnsClientRequest() {
+        assertThat(requestTrackerClientFilter.doWork(clientRequest), any(ClientRequest.class));
+    }
+
+    @Test
+    public void setsTheRequestTrackerHeader() {
+        requestTrackerClientFilter.doWork(clientRequest);
+        verify(headersMap).add(eq("X-Request-Tracker"), Mockito.any(UUID.class));
+    }
+
 }
