125ae3d6f95986603305758b9508a80b49843a45
Jon Radon <jon@radonfamily.com>
2015-04-21 19:52:33 -0400

    Refactor test structure to use rules

--- src/test/java/com/serviceenabled/dropwizardrequesttracker/it/BundleApplicationIT.java
+++ src/test/java/com/serviceenabled/dropwizardrequesttracker/it/BundleApplicationIT.java
@@ -4,66 +4,51 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import io.dropwizard.setup.Environment;
-import io.dropwizard.testing.junit.DropwizardAppRule;
 
-import java.net.URI;
 import java.util.UUID;
 
-import org.junit.BeforeClass;
+import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.ClassRule;
 import org.junit.Test;
 
-import com.serviceenabled.dropwizardrequesttracker.RequestTrackerClientFilter;
-import com.serviceenabled.dropwizardrequesttracker.RequestTrackerConfiguration;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+import org.junit.rules.RuleChain;
 
 
 public class BundleApplicationIT {
+	private static final DropwizardAppRule<BundleConfiguration> DROPWIZARD_APP_RULE = new DropwizardAppRule<BundleConfiguration>(BundleApplication.class, null);
+	private static final IntegrationTestSetupRule INTEGRATION_TEST_SETUP_RULE = new IntegrationTestSetupRule(DROPWIZARD_APP_RULE);
+
 	@ClassRule
-	public static final DropwizardAppRule<BundleConfiguration> RULE = new DropwizardAppRule<BundleConfiguration>(BundleApplication.class, null);
-	private static final Client CLIENT = new Client();
-	private static ClientTestResource CLIENT_TEST;
-	private static MockTestResource MOCK_TEST;
-	private static URI INITIAL_URI;
-	private static RequestTrackerConfiguration CONFIGURATION;
+	public static final RuleChain chain = RuleChain
+			.outerRule(DROPWIZARD_APP_RULE)
+			.around(INTEGRATION_TEST_SETUP_RULE);
 
-	@BeforeClass
-	public static void setupApplication() throws Exception {
-		CONFIGURATION = RULE.getConfiguration().getRequestTrackerConfiguration();
-		CLIENT.addFilter(new RequestTrackerClientFilter(CONFIGURATION));
-
-		INITIAL_URI = new URI("HTTP",null,"localhost",RULE.getLocalPort(),"/client-test",null,null);
-		URI secondaryURI = new URI("HTTP",null,"localhost",RULE.getLocalPort(),"/mock-test",null,null);
-
-		CLIENT_TEST = new ClientTestResource(secondaryURI, CLIENT);
-		MOCK_TEST = new MockTestResource();
-
-		RULE.getEnvironment().getApplicationContext().stop();
-		RULE.getEnvironment().jersey().register(MOCK_TEST);
-		RULE.getEnvironment().jersey().register(CLIENT_TEST);
-		RULE.getEnvironment().getApplicationContext().start();
-	}
-	
 	@Test
 	public void bundleAddsFilter(){
-		Environment e = RULE.getEnvironment();
+		Environment e = INTEGRATION_TEST_SETUP_RULE.getEnvironment();
+
 		assertThat(e.getApplicationContext().getServletHandler().getFilter("request-tracker-servlet-filter"), notNullValue());
 	}
 
 	@Test
 	public void addsTrackerToOutgoingRequest() throws Exception {
-		CLIENT.resource(INITIAL_URI).post(ClientResponse.class);
+		Client client = INTEGRATION_TEST_SETUP_RULE.getClient();
+		client.resource(INTEGRATION_TEST_SETUP_RULE.getInitialUri())
+				.post(ClientResponse.class);
 
-		assertThat(MOCK_TEST.getRequestTrackerId(), notNullValue());
+		assertThat(INTEGRATION_TEST_SETUP_RULE.getMockTestResource().getRequestTrackerId(), notNullValue());
 	}
 
 	@Test
 	public void keepsTheId() throws Exception {
 		Client client = new Client();
 		UUID id = UUID.randomUUID();
-		client.resource(INITIAL_URI).header(CONFIGURATION.getHeaderName(), id).post(ClientResponse.class);
+		client.resource(INTEGRATION_TEST_SETUP_RULE.getInitialUri())
+				.header(INTEGRATION_TEST_SETUP_RULE.getConfiguration().getHeaderName(), id)
+				.post(ClientResponse.class);
 
-		assertThat(MOCK_TEST.getRequestTrackerId(), equalTo(id.toString()));
+		assertThat(INTEGRATION_TEST_SETUP_RULE.getMockTestResource().getRequestTrackerId(), equalTo(id.toString()));
 	}
 }
--- src/test/java/com/serviceenabled/dropwizardrequesttracker/it/CustomIdSupplierBundleApplicationIT.java
+++ src/test/java/com/serviceenabled/dropwizardrequesttracker/it/CustomIdSupplierBundleApplicationIT.java
@@ -1,51 +1,30 @@
 package com.serviceenabled.dropwizardrequesttracker.it;
 
-import com.serviceenabled.dropwizardrequesttracker.RequestTrackerClientFilter;
-import com.serviceenabled.dropwizardrequesttracker.RequestTrackerConfiguration;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-
-import java.net.URI;
+import org.junit.rules.RuleChain;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 
 public class CustomIdSupplierBundleApplicationIT {
+	private static final DropwizardAppRule<BundleConfiguration> DROPWIZARD_APP_RULE = new DropwizardAppRule<BundleConfiguration>(CustomIdSupplierBundleApplication.class, null);
+	private static final IntegrationTestSetupRule INTEGRATION_TEST_SETUP_RULE = new IntegrationTestSetupRule(DROPWIZARD_APP_RULE);
+
 	@ClassRule
-	public static final DropwizardAppRule<BundleConfiguration> RULE = new DropwizardAppRule<BundleConfiguration>(CustomIdSupplierBundleApplication.class, null);
-	private static final Client CLIENT = new Client();
-	private static ClientTestResource CLIENT_TEST;
-	private static MockTestResource MOCK_TEST;
-	private static URI INITIAL_URI;
-	private static RequestTrackerConfiguration CONFIGURATION;
+	public static final RuleChain chain = RuleChain
+			.outerRule(DROPWIZARD_APP_RULE)
+			.around(INTEGRATION_TEST_SETUP_RULE);
 
-	@BeforeClass
-	public static void setupApplication() throws Exception {
-		CONFIGURATION = RULE.getConfiguration().getRequestTrackerConfiguration();
-		CLIENT.addFilter(new RequestTrackerClientFilter(CONFIGURATION));
-
-		INITIAL_URI = new URI("HTTP",null,"localhost",RULE.getLocalPort(),"/client-test",null,null);
-		URI secondaryURI = new URI("HTTP",null,"localhost",RULE.getLocalPort(),"/mock-test",null,null);
-
-		CLIENT_TEST = new ClientTestResource(secondaryURI, CLIENT);
-		MOCK_TEST = new MockTestResource();
-
-		RULE.getEnvironment().getApplicationContext().stop();
-		RULE.getEnvironment().jersey().register(MOCK_TEST);
-		RULE.getEnvironment().jersey().register(CLIENT_TEST);
-		RULE.getEnvironment().getApplicationContext().start();
-	}
-	
 	@Test
 	public void suppliesCustomId() throws Exception {
 		Client client = new Client();
-		client.resource(INITIAL_URI).post(ClientResponse.class);
+		client.resource(INTEGRATION_TEST_SETUP_RULE.getInitialUri()).post(ClientResponse.class);
 
-		assertThat(MOCK_TEST.getRequestTrackerId(), equalTo("12345"));
+		assertThat(INTEGRATION_TEST_SETUP_RULE.getMockTestResource().getRequestTrackerId(), equalTo("12345"));
 	}
 }
